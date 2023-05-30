d2af451d7616440406429294b63d0011c31c6b47
rileym666 <rileym3611@gmail.com>
2023-05-30 01:22:43 -0700

    initial

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def class_size():
+    return "hello world"
+
+@app.route('/termschedule',methods=['POST'])
+def schedule():
+    return "v2"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
