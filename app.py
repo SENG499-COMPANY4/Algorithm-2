aa896009e1fd5ac0a4f7b76b3b6bb796649a0f4b
Stefan Esqivel <sesquivel@uvic.ca>
2023-06-16 15:11:16 -0700

    Made changes

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from class_size_predictor import returnClassSize
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return "Hello World!"
+
 
 @app.route('/predict_class_sizes', methods=['GET', 'POST'])
 def class_size():
     if request.method == 'GET':
         return returnClassSize()
     elif request.method == 'POST':
-        return "v2" # TODO: make function to save data
+        return "v2"  # TODO: make function to save data
 
 # if __name__ == "__main__":
 #     app.run(debug=True, host='0.0.0.0', port=5000)
