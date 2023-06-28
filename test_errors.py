4c3032839be13757ec2e31b0d414d39a26c270e0
Karen McDougall <karenmcdougall@uvic.ca>
2023-06-28 10:34:22 -0700

    Correct error code test cases and add cases for testing GET, POST to wrong URL and internal errors from returnClassSize() function

--- test_errors.py
+++ test_errors.py
@@ -10,6 +10,15 @@ class api_tests(unittest.TestCase):
         app.config["TESTING"] = True
         self.app = app.test_client()
 
+    def test_get(self):
+        print("Testing GET")
+        response = self.app.get("/predict_class_sizes") 
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(
+            response.data,
+            b'{"error":"405 Method Not Allowed: The method is not allowed for the requested URL."}\n'
+        )
+
     def test_get_to_wrong_URL(self):
         print("Testing GET to wrong URL")
         response = self.app.get("/predict_class_sizes/1") 
@@ -19,7 +28,16 @@ class api_tests(unittest.TestCase):
             b'{"error":"404 Not Found: The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again."}\n'
         )
     
-    def test_no_json_file(self):
+    def test_post_to_wrong_URL(self):
+        print("Testing POST to wrong URL")
+        response = self.app.post("/predict_class_sizes/predict_class_sizes")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.data,
+            b'{"error":"404 Not Found: The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again."}\n'
+        )
+    
+    def test_post_no_json_file(self):
         print("Testing POST with no json file")
         response = self.app.post("/predict_class_sizes")
         self.assertEqual(response.status_code, 415)
@@ -28,8 +46,7 @@ class api_tests(unittest.TestCase):
             b'{"error":"415 Unsupported Media Type: Did not attempt to load JSON data because the request Content-Type was not \'application/json\'."}\n'
         )
 
-
-    def test_None_json_file(self):
+    def test_post_None_json_file(self):
         print("Testing POST with None json file")
         response = self.app.post("/predict_class_sizes", json=None)
         self.assertEqual(response.status_code, 415)
@@ -39,6 +56,23 @@ class api_tests(unittest.TestCase):
         )
     
     # TODO: Add test case where POST gives file in incorrect format then GET should produce 500 error
+    def test_post_incorrect_file_not_list(self):
+        print("Testing POST with incorrect file format (not a list)")
+        response = self.app.post("/predict_class_sizes", json="./incorrect_file_format.json")
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(
+            response.data,
+            b'{"error":"500 Internal Error: string indices must be integers"}\n'
+        )
+
+    def test_post_incorrect_file(self):
+        print("Testing POST with incorrect file format (missing \"course\")")
+        response = self.app.post("/predict_class_sizes", json="./incorrect_file_format_2.json")
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(
+            response.data,
+            b'{"error":"500 Internal Error: string indices must be integers"}\n'
+        )
 
 if __name__ == "__main__":
     unittest.main()
