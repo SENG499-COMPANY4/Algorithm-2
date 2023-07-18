fdc7a174a365ff24e9214dc6402e1b1c99b800e5
Karen McDougall <karenmcdougall@uvic.ca>
2023-07-18 14:52:05 -0700

    Modify setup.py and revert imports back in test_endpoint, test_errors and test_regression

--- test/setup.py
+++ test/setup.py
@@ -1,6 +1,14 @@
-# Add the parent directory to the Python path
 import sys
-sys.path.append('..')
+import os
 
-# Import the app module from app.py
+# Get the name of the directory where the file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Get the parent directory name where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# Add the parent directory to the sys.path.
+sys.path.append(parent)
+ 
+# Import the module in the parent directory.
 from app import app
--- test/test_endpoint.py
+++ test/test_endpoint.py
@@ -1,4 +1,4 @@
-from ..app import app
+from setup import app
 import json
 import unittest
 
